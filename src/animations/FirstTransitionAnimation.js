b21683c5ca978f91e0d55be7dbe22b4b7e93fbe1
matiasberrios <matiasberrios@chromium.org>
2023-05-13 22:05:31 -0400

    pages

--- src/animations/FirstTransitionAnimation.js
+++ src/animations/FirstTransitionAnimation.js
@@ -1,11 +1,6 @@
-let Typed = window.Typed;
+
+
 
 export default function FirstTransitionAnimation() {
-  new Typed("#typewriter", {
-    strings: [" "],
-    typeSpeed: 100,
-    backSpeed: 100,
-    smartBackspace: true, // this is a default
-    loop: true,
-  });
+
 }
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="nav-header px-5">
-    <div class="locales-container"><a>es</a>/<a>en</a></div>
+    <div class="locales-container"><a id="es">es</a>/<a id="en">en</a></div>
   </header>
 </template>
