d1033a096545366ffe53bfaad1ef683511bd16d1
matiasberrios <matiasberrios@chromium.org>
2023-05-15 23:00:47 -0400

    gh-pages

--- src/animations/AboutMeEnterTransition.js
+++
@@ -1,8 +0,0 @@
-import { gsap } from "gsap";
-
-export default function AboutMeEnterTransition() {
-  gsap.to(".aboutme-text", {
-    delay: 0.5,
-    opacity: 1,
-  });
-}
--- src/components/NavHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <header class="nav-header px-5">
-    <div class="header-content w-100  d-flex">
-      matias berrios<div class="locales-container"><a id="es">es</a>/<a id="en">en</a></div>
-    </div>
-  </header>
-</template>
