6d42c40a0e9e1f5227f383c9d969924d24a5211c
matiasberrios <matiasberrios@chromium.org>
2023-05-28 01:39:53 -0400

    nice

---
+++ src/components/FirstVolume.vue
@@ -0,0 +1,28 @@
+<template>
+  <GeneralVolumeVue
+    Id = "first-volume"
+    Role="> Riolab"
+    Description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quia, quos, quod voluptate quas voluptatibus quibusdam quae voluptates doloribus quidem."
+    From="Marzo 2023"
+    To="Actualidad"
+    HeaderMethod="FirstVolume()"
+  ></GeneralVolumeVue>
+
+  <div class="col-9 volume-company position-absolute">
+    <!-- img from picsum -->
+    <div
+      data-scroll
+      data-scroll-speed="1"
+      data-scroll-delay="0.18"
+      data-scroll-direction="horizontal"
+      class="center"
+    >
+      <BlobOne />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import BlobOne from "@/assets/svgs/BlobOne.vue";
+import GeneralVolumeVue from "./GeneralVolume.vue";
+</script>
---
+++ src/components/ThirdVolume.vue
@@ -0,0 +1,14 @@
+<template>
+  <GeneralVolumeVue
+    Role="> Skipo"
+    Description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quia, quos, quod voluptate quas voluptatibus quibusdam quae voluptates doloribus quidem."
+    From="Marzo 2023"
+    To="Actualidad"
+    HeaderMethod="FirstVolume()"
+    Id="third-volume"
+  ></GeneralVolumeVue>
+</template>
+
+<script setup>
+import GeneralVolumeVue from "./GeneralVolume.vue";
+</script>
